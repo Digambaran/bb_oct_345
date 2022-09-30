171ee009cd7a9418f5fa5af876dbd3a374827ef2
Digambaran <arjunsreekumar.1@gmail.com>
2022-09-30 14:50:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Bb_oct_345 from './bb_oct_345'
+export default function App() {
+  return (
+    <div>
+      <Bb_oct_345/>
+    </div>
+  )
+}
---
+++ src/bb_oct_345.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Bb_oct_345 = () => {
+  return (
+   <p>Hello from bb_oct_345 </p>
+  )
+}
+export default Bb_oct_345
